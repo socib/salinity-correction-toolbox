18d0992ac5bca9d51b0146fa19a397df55d55f9c
Cristian Munoz <cmunoz@socib.es>
2019-11-14 12:04:17 +0100

    Initial commit

---
+++ lib/glider-salinity-correction-pack/res/config/glider_load_sections_idx.m
@@ -0,0 +1,12 @@
+function idxToProcess = glider_load_sections_idx()
+
+
+%% dep0001_sdeep05_scb-sldeep005_L1_corr_2017-03-03 sections
+%idxToProcess = [1, 807051, 842251, 846501,852001, 870001,930001, 947001, 956001, 972001, 1012001, 1158292, 1162501, 1185001, 1210001, 1300001, 1505001,1840001, 2550001, 2615737];
+
+%% dep0002_sdeep05_scb-sldeep005_L1_2017-11-16 sections
+%idxToProcess = [1, 245201, 376501, 615101, 772869];
+
+%% dep0003_sdeep05_scb-sldeep005_L1_2018-05-09 sections
+% idxToProcess = [1, 30651, 49201, 64579];
+end
---
+++ res/ops/import_instrument_ids.m
@@ -0,0 +1,6 @@
+function InstrumentID = import_instrument_ids()
+
+InstrumentID.ctdInstrumentID = [172, 448, 640, 642, 650];
+InstrumentID.gliderInstrumentID = [30, 195, 46, 48, 196, 197, 568, 569];
+InstrumentID.argoInstrumentID = [134, 267, 387, 388, 389, 391, 588, 656];
+end
